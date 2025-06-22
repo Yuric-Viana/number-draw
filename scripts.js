aaca8dbd4b251e97c019f15b5c150e29e64c0549
Yuri Viana <ycviana@sga.pucminas.br>
2025-06-22 15:58:36 -0300

    feat: adicionar efeito quando o input estiver em foco

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// const rotateBox = document.querySelector('.rotate-box')
+// const resultDraw = document.querySelector('.result .draw-button')
+
+// rotateBox.addEventListener('animationend', () => {
+//     resultDraw.style.visibility = 'visible'
+//     resultDraw.style.animation = 'buttonUp 4s both'
+// })  
